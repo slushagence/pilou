26d8384402a86683392e8dec0331ef0787c14463
SLUSHAGENCE <marc@slush.fr>
2026-06-24 18:30:21 +0200

    fix: centrer lien lapilou.fr

--- src/pages/Jeu.jsx
+++ src/pages/Jeu.jsx
@@ -100,7 +100,7 @@ export default function Jeu() {
           draggable="false" />
 
         <a href="https://www.lapilou.fr" target="_blank" rel="noopener noreferrer"
-          className="mt-4 text-base font-bold text-pilou-creme uppercase underline hover:opacity-80">
+          className="mt-4 text-base font-bold text-pilou-creme uppercase underline hover:opacity-80 text-center block">
           🍺 <i>Qu'es la Pilou ?</i> 🍺<br/>VISITEZ NOTRE SITE POUR EN SAVOIR PLUS !
         </a>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -41,7 +41,7 @@ export default function Landing() {
         href="https://www.lapilou.fr"
         target="_blank"
         rel="noopener noreferrer"
-        className="mt-4 text-base font-bold text-pilou-rouge uppercase underline hover:opacity-80"
+        className="mt-4 text-base font-bold text-pilou-rouge uppercase underline hover:opacity-80 text-center block"
       >
         🍺 <i>Qu'es la Pilou ?</i> 🍺<br/>VISITEZ NOTRE SITE POUR EN SAVOIR PLUS !
       </a>
